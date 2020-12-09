08fd01a2d4feb980f70bec2a81f1c09f1323231f
Abhisheknishant <66300308+Abhisheknishant@users.noreply.github.com>
2020-12-09 16:30:01 +0530

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
-if [ $GOARCH = "arm64" ]; then
-  export CC=/usr/bin/aarch64-linux-gnu-gcc
-else
-  export CC=/usr/bin/musl-gcc
+set -e
+rm -rf completions
+mkdir completions
+if [ $GOOS = "linux" ]; then
+  for arch in amd64 arm64; do
+  if [ $GOARCH = "arm64" ]; then
+    export CC=/usr/bin/aarch64-linux-gnu-gcc >"completions/goreleaser.$arch"
+  else
+    export CC=/usr/bin/musl-gcc >"completions/goreleaser.$arch"
+  fi
 fi
